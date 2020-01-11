e98699aacda29fe79a32e34dcb454d16041a2321
hkmalladi <hkmalladi@gmail.com>
2020-01-11 13:28:36 +0530

    improvements

--- sort_into_albums.py
+++ sort_into_albums.py
@@ -3,6 +3,12 @@ import json
 import os
 import glob
 import shutil
+
+list_of_files = glob.glob('*.json')
+for filename in list_of_files:
+   os.remove(filename)
+quit()
+
 with open('albums.json') as f:
   data = json.load(f)
   album_list = data.get('albums')
@@ -12,9 +18,16 @@ with open('albums.json') as f:
         print('PROCESSING: ' + title)
         folder =  title.replace(' ', '_').replace('<','LT').replace('>','GT')
         try:
-            os.mkdir(folder)
-        except:
+            if not os.path.exists(folder):
+                os.mkdir(folder)
+        except Exception as e:
             print('Folder ', folder, ' exists')
+            print('Exception: ', e)
+        for pic in pics:
+            list_of_files = glob.glob('photo_' + pic + '.json')
+            for filename in list_of_files:
+                  os.remove(filename)
+
         for pic in pics:
             list_of_files = glob.glob('*_' + pic + '_*')
             for filename in list_of_files:
@@ -22,5 +35,31 @@ with open('albums.json') as f:
                 try:
                     shutil.move(filename, folder)
                     print('mv ' + filename + ' ' + folder)
-                except:
+                except Exception as e:
                     print('mv ' + filename + ' ' + folder + ' FAILED')
+                    print('Exception: ', e)
+        for pic in pics:
+            list_of_files = glob.glob(pic + '_*')
+            for filename in list_of_files:
+                filename = filename.strip('\n')
+                try:
+                    shutil.move(filename, folder)
+                    print('mv ' + filename + ' ' + folder)
+                except Exception as e:
+                    print('mv ' + filename + ' ' + folder + ' FAILED')
+                    print('Exception: ', e)
+        for pic in pics:
+            list_of_files = glob.glob('*_' + pic + '.*')
+            for filename in list_of_files:
+                filename = filename.strip('\n')
+                try:
+                    shutil.move(filename, folder)
+                    print('mv ' + filename + ' ' + folder)
+                except Exception as e:
+                    print('mv ' + filename + ' ' + folder + ' FAILED')
+                    print('Exception: ', e)
+
+for pic in pics:
+    list_of_files = glob.glob('*.json')
+    for filename in list_of_files:
+        os.remove(filename)
